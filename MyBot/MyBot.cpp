3bc8f3c59164a3fadba9f461b2d6efd1e365cdbf
Kishan Chauhan <mtcup2000@yahoo.com>
2024-01-05 12:20:36 -0600

    Created Copy Bot with basic message copy feature

--- MyBot/MyBot.cpp
+++ MyBot/MyBot.cpp
@@ -4,41 +4,36 @@
 /* Be sure to place your token in the line below.
  * Follow steps here to get a token:
  * https://dpp.dev/creating-a-bot-application.html
- * When you invite the bot, be sure to invite it with the 
+ * When you invite the bot, be sure to invite it with the
  * scopes 'bot' and 'applications.commands', e.g.
  * https://discord.com/oauth2/authorize?client_id=940762342495518720&scope=bot+applications.commands&permissions=139586816064
  */
-const std::string    BOT_TOKEN    = "add your token here";
+const std::string    BOT_TOKEN = "YOUR TOKEN";
 
-int main()
-{
-	/* Create bot cluster */
-	dpp::cluster bot(BOT_TOKEN);
+const dpp::snowflake source_channel_id = 11111111111; // Change to your source channels ID. This will be the channel that messages get copied from
 
-	/* Output simple log messages to stdout */
+const std::vector<dpp::snowflake> target_channel_ids = { 1111111111 }; //Add your target channels. These will be the channels the messages get copied too.
+
+
+int main() {
+	/* Create the bot, but with our intents so we can use messages. */
+	dpp::cluster bot(BOT_TOKEN, dpp::i_default_intents | dpp::i_message_content);
+
 	bot.on_log(dpp::utility::cout_logger());
 
-	/* Register slash command here in on_ready */
-	bot.on_ready([&bot](const dpp::ready_t& event) {
-		/* Wrap command registration in run_once to make sure it doesnt run on every full reconnection */
-		if (dpp::run_once<struct register_bot_commands>()) {
-			std::vector<dpp::slashcommand> commands {
-				{ "ping", "Ping pong!", bot.me.id }
-			};
+	bot.on_message_create([&bot](const dpp::message_create_t& event) {
+		if (event.msg.channel_id == source_channel_id)
+		{
+			std::cout << "Message source: " << event.msg.channel_id << std::endl;
 
-			bot.global_bulk_command_create(commands);
+			for (const auto& target_channel_id : target_channel_ids)
+			{
+				dpp::message msg(target_channel_id, event.msg.content);
+				bot.message_create(msg);
+			}
 		}
-	});
+		});
 
-	/* Handle slash command with the most recent addition to D++ features, coroutines! */
-	bot.on_slashcommand([](const dpp::slashcommand_t& event) -> dpp::task<void> {
-		if (event.command.get_command_name() == "ping") {
-			co_await event.co_reply("Pong!");
-		}
-		co_return;
-	});
-
-	/* Start the bot */
 	bot.start(dpp::st_wait);
 
 	return 0;
